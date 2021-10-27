5c46bada298d3747639c40791f1209fda68b1d94
TheAndrewHG <92337587+TheAndrewHG@users.noreply.github.com>
2021-10-27 10:06:29 -0500

    Nombre y Matricula

---
+++ Practica en PHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primera practica en PHP</title>
+</head>
+<body>
+    <?php
+    $mivariable;
+    $variablematricula;
+    $mivariable = "Luis Fernando Hernandez Garcia";
+    $variablematricula ="1320114059";
+    echo "Hola mundo".$mivariable;
+    echo "<br><input type text value= '$variablematricula'>";
+    ?>
+</body>
+</html>
